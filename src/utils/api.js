bbefbba324871b7ab62567f99a90b987ab31196d
jdreyespaez <jd.reyespaez@gmail.com>
2016-05-27 21:11:04 +0700

    S8. P1. Con getNotes(username) hacemos GET de usuario. P2. addNote(username, note) hacemos POST de una nota para un usuario particular

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,6 +8,21 @@ var api = {
     username = username.toLowerCase().trim();
     var url = `https://api.github.com/users/${username}/repos`;
     return fetch(url).then((res) => res.json());
+  },
+  // S8.P1 Pedir un usuario
+  getNotes(username){
+    username = username.toLowerCase().trim();
+    var url = `https://github-social.firebaseio.com/${username}.json`;
+    return fetch(url).then((res) => res.json());
+  },
+  // S8.P2 Crear un usuario
+  addNote(username, note){
+    username = username.toLowerCase().trim();
+    var url = `https://github-social.firebaseio.com/${username}.json`;
+    return fetch(url, {
+      method: 'post',
+      body: JSON.stringify(note)
+    }).then((res) => res.json());
   }
 };
 
