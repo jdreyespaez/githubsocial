dc1ef30dfa8d160d6a1a59f352fc0c1b3edfe21b
jdreyespaez <jd.reyespaez@gmail.com>
2016-05-27 13:35:10 +0700

    S5.P6. Formatear estilos e incluir la lista

--- src/components/profile.js
+++ src/components/profile.js
@@ -10,7 +10,24 @@ import {
 
 class Profile extends React.Component{
   render(){
-    
+    var userInfo = this.props.userInfo;
+    var topicArr = ['company', 'location', 'followers', 'following', 'email', 'bio', 'public_repos'];
+    var list = topicArr.map((item, index) => {
+      // Lo que está vacío. No todos lo completan igual su gitHub.
+      if(!userInfo[item]){
+        return <View key={index} />
+      } else {
+        <View key={index}>
+          <View style={styles.rowContainer}>
+            <Text style={styles.rowTitle}> /* Aquí irá Company o Location */ </Text>
+            <Text style={styles.rowContent}> {userInfo[item]} </Text>
+          </View>
+        </View>
+      }
+    });
+    <ScrollView style={styles.container}>
+      <Badge userInfo={this.props.userInfo} />
+    </ScrollView>
   }
 };
 
